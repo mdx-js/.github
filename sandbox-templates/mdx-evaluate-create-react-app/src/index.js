d08bf8c6113a0b91d6ce394a837e64511acf6ff8
Christian Murphy <christian.murphy.42@gmail.com>
2021-11-16 12:55:41 -0700

    add extension to imports in sandbox starter, remove unneeded escapes

--- sandbox-templates/mdx-evaluate-create-react-app/src/index.js
+++ sandbox-templates/mdx-evaluate-create-react-app/src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "./App.js";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
