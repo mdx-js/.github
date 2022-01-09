648429acf948bd47c44a259e5f9e9749be7b02a1
Christian Murphy <christian.murphy.42@gmail.com>
2022-01-09 07:33:42 -0700

    docs: configure next with an mjs file
    this makes it easier to load ESM packages like remark and rehype plugins

--- sandbox-templates/mdx-loader-next/next.config.mjs
+++ sandbox-templates/mdx-loader-next/next.config.mjs
@@ -1,7 +1,7 @@
 // ⚠️ Important! Please make sure the dependencies are up to date.
 // You can refresh them in the Dependencies section (left-bottom on CodeSandbox)
 
-module.exports = {
+const configuration = {
   // Support MDX files as pages:
   pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
   // Support loading `.md`, `.mdx`:
@@ -24,3 +24,5 @@ module.exports = {
     return config;
   },
 };
+
+export default configuration;
