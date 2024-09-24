4e2032d5a92593cac63ffa229a4ae9fd334284f0
Titus Wormer <tituswormer@gmail.com>
2024-09-24 13:06:41 +0200

    Update Node + `@mdx-js/mdx` example

--- sandbox-templates/mdx-evaulate-node/index.js
+++ sandbox-templates/mdx-evaulate-node/index.js
@@ -2,16 +2,17 @@
 // You can refresh them in the Dependencies section (left-bottom on CodeSandbox)
 
 import http from 'node:http'
-import * as runtime from 'react/jsx-runtime'
-import ReactDom from 'react-dom/server'
 import {evaluate} from '@mdx-js/mdx'
+import {renderToPipeableStream} from 'react-dom/server'
+import * as runtime from 'react/jsx-runtime'
 
 // Note: refresh the pane on the right to see changes.
 
 http
   .createServer(async function (_, response) {
-    const content = "Hi <span style={{color: 'tomato'}}>there</span>!"
-    const {default: Content} = await evaluate(content, runtime)
-    ReactDom.renderToNodeStream(Content()).pipe(response)
+    const mdx = "Hi <span style={{color: 'tomato'}}>there</span>!"
+    const {default: Content} = await evaluate(mdx, runtime)
+    const {pipe} = renderToPipeableStream(Content({}))
+    pipe(response)
   })
   .listen(8080)
